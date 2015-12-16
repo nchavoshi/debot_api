9173f7d9ec020e0056585cd4d8fc05c4f2c40383
nchavoshi <nikan.chavoshi@gmail.com>
2015-12-16 13:31:43 -0700

    Create __init__.py

---
+++ debot/__init__.py
@@ -0,0 +1,20 @@
+import requests
+
+class DeBot(object):
+	
+	
+	def __init__(self, api_key):
+		self.api_key = api_key	
+		self._api_endpoint = 'http://cs.unm.edu/~chavoshi/demo/api.php'
+
+	def check_user(self, screen_name):
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'1', 'user_name':screen_name})
+		return r.text
+		
+	def get_bots_date_range(self, from_date, to_date):
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'2', 'date_1':from_date, 'date_2':to_date})
+		return r.text
+		
+	
+			
+		
