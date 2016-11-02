591f514a00f8dac7020fa98de94b079711d7b9c8
nchavoshi <nikan.chavoshi@gmail.com>
2016-11-02 11:16:16 -0600

    Three more functions

--- debot/__init__.py
+++ debot/__init__.py
@@ -7,12 +7,20 @@ class DeBot(object):
 		self.api_key = api_key	
 		self._api_endpoint = 'http://cs.unm.edu/~chavoshi/debot/api.php'
 
+	def daily_debot(self, limit=5000):
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'1', 'limit':limit})
+		return r.text
+		
 	def check_user(self, screen_name):
-		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'1', 'user_name':screen_name})
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'2', 'user_name':screen_name})
 		return r.text
 		
 	def get_bots_date_range(self, from_date, to_date):
-		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'2', 'date_1':from_date, 'date_2':to_date})
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'3', 'date_1':from_date, 'date_2':to_date})
+		return r.text
+		
+	def get_user_threshold(self, threshold, overall=1):
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'4', 'threshold':threshold, 'overall':overall})
 		return r.text
 		
 	
