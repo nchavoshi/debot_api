d5923f95fc7b04f96e9392a60acb5adea5983a76
nchavoshi <nikan.chavoshi@gmail.com>
2016-11-11 14:36:18 -0700

    Update __init__.py

--- debot/__init__.py
+++ debot/__init__.py
@@ -18,6 +18,10 @@ class DeBot(object):
 	def get_bots_date_range(self, from_date, to_date):
 		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'3', 'date_1':from_date, 'date_2':to_date})
 		return r.text
+	
+	def get_bots_list(self, _date):
+		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'3', 'date_1':_date, 'date_2':_date})
+		return r.text
 		
 	def get_user_threshold(self, threshold, overall=1):
 		r = requests.post(self._api_endpoint, data={'api_key':self.api_key, 'srv_type':'4', 'threshold':threshold, 'overall':overall})
